aa3ebdfc1a74c695f1a8724c85d56ba244c1bb99
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-21 14:23:20 +0700

    save, update, and remove document from database

--- index.js
+++ index.js
@@ -17,16 +17,62 @@ mongoose.connect('mongodb://localhost/latihan1', {useNewUrlParser: true})
 
   async function createArtikel() {
     const artikel = new Artikel({
-      nama: 'Dimas',
-      nim: 09108244034,
-      email: 'dimasmillaty@gmail.com',
-      judul: 'No SQL Database',
-      tags: ['mongo.db', 'backend'],
-      isPublished: true
+      nama: 'Alita',
+      nim: 09108244557,
+      email: 'zarazar@gmail.com',
+      judul: 'Make modern website with HTML, CSS, JS',
+      tags: ['javascript', 'frontend'],
+      isPublished: false
     });
 
     const result = await artikel.save();
     console.log(result);
   }
 
-  createArtikel();
+  // createArtikel();
+
+  async function getArtikel() {
+    const artikel = await Artikel
+    .find({ isPublished: false})
+    .select();
+    
+      console.log(artikel);
+  }
+
+  // getArtikel();
+
+  // async function updateArtikel(id) {
+  //   const result = await Artikel.update({ _id: id }, {
+  //     $set: {
+  //       email: 'litaalit@gmail.com'
+  //     }
+  //   });
+  //   console.log(result);
+  // }
+
+  // // // untuk edit sekaligus menampilkan data yang di update pakai ini:
+  // // async function updateArtikel(id) {
+  // //   const artikel = await Artikel.findByIdAndUpdate( id, {
+  // //     $set: {
+  // //       email: 'litaalit@gmail.com'
+  // //     }
+  // //   }, { new: true });
+  // //   console.log(artikel);
+  // // }
+
+  // updateArtikel('5d5cdd805db463174808592a');
+
+
+// REMOVE______________________________________________________________
+ 
+// async function removeArtikel(id) {
+//   const result = await Artikel.deleteOne({ _id: id });
+//   console.log(result);
+// }
+
+async function removeArtikel(id) {
+  const artikel = await Artikel.findByIdAndRemove(id);
+  console.log(artikel);
+}
+
+removeArtikel('5d5ce2bbdf09401210734d91');
